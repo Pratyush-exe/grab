249273910a56061dcb54f7fce892b91e94326e79
Vasu Dutt <2vasudutt@gmail.com>
2022-06-19 20:01:14 +0530

    tailwind config fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,26 +3,27 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      height: theme => ({
-        "500px": "500px",
-        "400px": "400px",
-        "30px": "30px",
-        "45px": "45px",
-        "100px": "100px",
-        "95vh": "90vh",
-        "5vh": "9vh"
+      height: (theme) => ({
+        '500px': '500px',
+        '400px': '400px',
+        '30px': '30px',
+        '45px': '45px',
+        '100px': '100px',
+        '95vh': '90vh',
+        '5vh': '9vh',
       }),
-      width: theme => ({
-        "600px": "600px",
-        "400px": "400px",
-        "198px":"198px",
+      width: (theme) => ({
+        '600px': '600px',
+        '400px': '400px',
+        '198px': '198px',
       }),
-      fontFamily: theme => ({
-        "ubuntu": "Montserrat"
-      })
+      fontFamily: (theme) => ({
+        ubuntu: 'Montserrat',
+      }),
+      colors: {
+        primary: '#ffdb18',
+      },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar')
-  ],
+  plugins: [require('tailwind-scrollbar')],
 };
