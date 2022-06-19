269b1cd4548d90e576bbeb5a37f59945c3162afa
Vasu Dutt <2vasudutt@gmail.com>
2022-06-19 18:32:34 +0530

    About Page

---
+++ frontend/src/About/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { AiFillGithub, AiFillCode } from 'react-icons/ai';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaUniversity } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
+import { TeamData } from './teamData';
+
+const Card = ({
+  name,
+  imgUrl,
+  prof,
+  loc,
+  college,
+  github,
+  linkedin,
+}) => {
+  return (
+    <div className="bg-primary rounded-xl mx-auto max-w-xs mb-8">
+      <img className="rounded-t-xl" src={imgUrl} alt="Profile" />
+
+      <div className="py-2 px-4 flex justify-between items-center border-b-2 border-black">
+        <h2 className="font-semibold text-lg">{name}</h2>
+
+        <div className="flex items-center text-xl">
+          <a className="pr-2" href={github}>
+            <AiFillGithub />
+          </a>
+          <a href={linkedin}>
+            <BsLinkedin />
+          </a>
+        </div>
+      </div>
+
+      <ul className="py-2 px-4">
+        <li className="flex justify-start items-center">
+          <AiFillCode className="mr-2" /> {prof}
+        </li>
+        <li className="flex justify-start items-center">
+          <MdLocationOn className="mr-2" /> {loc}
+        </li>
+        <li className="flex justify-start items-center">
+          <FaUniversity className="mr-2" /> {college}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <h1 className="p-4 font-bold text-xl">Our Team</h1>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {TeamData.map(
+          ({
+            key,
+            name,
+            imgUrl,
+            prof,
+            loc,
+            college,
+            github,
+            linkedin,
+          }) => (
+            <Card
+              key={key}
+              imgUrl={imgUrl}
+              name={name}
+              prof={prof}
+              loc={loc}
+              college={college}
+              github={github}
+              linkedin={linkedin}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ frontend/src/About/teamData.js
@@ -0,0 +1,37 @@
+import ronit from './images/ronit.jpg';
+import vasu from './images/vasu.jpg';
+import pratyush from './images/pratyush.jpg';
+
+export const TeamData = [
+  {
+    key: 0,
+    imgUrl: vasu,
+    name: 'Vasu Dutt',
+    prof: 'Web Dev',
+    loc: 'Ghaziabad, India',
+    college: 'KIIT Bhubaneswar',
+    github: 'https://github.com/vasudutt',
+    linkedin: 'https://www.linkedin.com/in/vasu-dutt/',
+  },
+  {
+    key: 1,
+    imgUrl: pratyush,
+    name: 'Pratyush Patnaik',
+    prof: 'ML/AI, Web Dev',
+    loc: 'Bhubaneswar, India',
+    college: 'KIIT Bhubaneswar',
+    github: 'https://github.com/Pratyush-exe',
+    linkedin:
+      'https://www.linkedin.com/in/pratyush-patnaik-1394a91b8/',
+  },
+  {
+    key: 2,
+    imgUrl: ronit,
+    name: 'Ronit Kamilla',
+    prof: 'Web Dev',
+    loc: 'Bhubaneswar, India',
+    college: 'KIIT Bhubaneswar',
+    github: 'https://github.com/ronit01',
+    linkedin: 'https://www.linkedin.com/in/ronit-kamilla-981433215/',
+  },
+];
